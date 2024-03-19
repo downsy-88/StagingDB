72781779a0c9408ea53471d5118ad70a7bd01616
Chris Downs <chris.downs@pricecheck.uk.com>
2024-03-19 15:46:46 +0000

    Add test column

--- Tables/dbo.ASIN.sql
+++ Tables/dbo.ASIN.sql
@@ -10,7 +10,8 @@ CREATE TABLE [dbo].[ASIN]
 [ASIN] [varchar] (20) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
 [SetPieces] [int] NULL,
 [Price] [float] NOT NULL,
-[LastRunTimeETL] [datetime] NULL
+[LastRunTimeETL] [datetime] NULL,
+[test] [nchar] (10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
 )
 GO
 ALTER TABLE [dbo].[ASIN] ADD CONSTRAINT [ASIN_ID] PRIMARY KEY CLUSTERED ([ID])
