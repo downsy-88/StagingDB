635f729fb0902251005214041053823fa08e03cd
Chris Downs <chris.downs@pricecheck.uk.com>
2024-03-19 15:06:53 +0000

    Add ASIN table to Staging TBL

---
+++ Tables/dbo.ASIN.sql
@@ -0,0 +1,17 @@
+CREATE TABLE [dbo].[ASIN]
+(
+[ID] [int] NOT NULL IDENTITY(1, 1),
+[VendorCode] [varchar] (20) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+[ExactProduct] [varchar] (20) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[Description] [varchar] (max) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[ProductPreperation] [varchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[Barcode] [varchar] (20) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+[CustomBarcode] [varchar] (20) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[ASIN] [varchar] (20) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+[SetPieces] [int] NULL,
+[Price] [float] NOT NULL,
+[LastRunTimeETL] [datetime] NULL
+)
+GO
+ALTER TABLE [dbo].[ASIN] ADD CONSTRAINT [ASIN_ID] PRIMARY KEY CLUSTERED ([ID])
+GO
